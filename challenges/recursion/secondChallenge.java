26c10d4b0d7134cd3b07ec4902bc65f51909ac4b
orchlonn <ch.orchlon@gmail.com>
2025-01-14 14:13:57 -0800

    second challenge

---
+++ challenges/recursion/secondChallenge.java
@@ -0,0 +1,9 @@
+public class secondChallenge {
+    public static int secondChallenge(String word){
+        return word.length();
+    };
+    
+    public static void main(String[] args) {
+        System.out.println(secondChallenge("abcdef"));
+    }
+}
