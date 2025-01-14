cf1637a81153dcb6b4dfb95c831d013c61e7eb5b
orchlonn <ch.orchlon@gmail.com>
2025-01-14 14:57:16 -0800

    bug fixed

--- challenges/recursion/secondChallenge.java
+++ challenges/recursion/secondChallenge.java
@@ -1,9 +1,9 @@
 public class secondChallenge {
-    public static int secondChallenge(String word){
+    public static int findLength(String word){
         return word.length();
     };
     
     public static void main(String[] args) {
-        System.out.println(secondChallenge("abcdef"));
+        System.out.println(findLength("abcdef"));
     }
 }
